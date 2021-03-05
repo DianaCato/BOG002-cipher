e99e519fa711de61fa2902bb883ee74ec43be782
DianaCato <diana.catolico@hotmail.com>
2021-03-05 18:25:32 -0500

    array con númerol y vocales con tilde

--- src/cipher.js
+++ src/cipher.js
@@ -7,13 +7,26 @@ const cipher = {
 
       let firstAnswer=[];                                          //array con las respuestas sin codificar
       let answerInCesar=[];
+      const arrayNumbers =['7','8','9','0','Ó','Á','É','Í','Ú','Ñ','1','2','3','4','5','6'];
+
       
       for (let i=0;i<string.length;i++)
         {
-           if (string.charCodeAt (i) <= 64 || string.charCodeAt (i) >= 91)    //Si se insertar valores diferentes a letras, no serán codificados
+           if (string.charCodeAt (i) <= 64 || string.charCodeAt (i) >= 91)    //Si se insertar valores como 'Ñ', letras con tilde o números, se codifican empleando un array
            {
-              answerInCesar.push(string.charAt (i));
+            let positionInitial =arrayNumbers.indexOf(string.charAt (i)); 
+
+            if (positionInitial >= 0) {
+              let positionFinal = (positionInitial + (offSet % arrayNumbers.length))% arrayNumbers.length;
+              answerInCesar.push (arrayNumbers[positionFinal]);
+              
+            }
+            else 
+            {
+              answerInCesar.push(string.charAt(i));                             //Si se insertar valores diferentes a letras o números, no serán codificados
+            }
            }
+           
            else 
            {
               firstAnswer [i]=(string.charCodeAt(i)- 65 + offSet)%26+ 65;
@@ -34,12 +47,32 @@ const cipher = {
     {
       let answerCipher=[];
       let answerDecode=[];
+      const arrayNumbersDecode =['7','8','9','0','Ó','Á','É','Í','Ú','Ñ','1','2','3','4','5','6'];
+
 
       for (let i=0;i<string.length;i++)
       {          
-        if (string.charCodeAt (i) <= 64 || string.charCodeAt (i) > 90)    //Si se insertar valores diferentes a letras, no serán codificados
-        {
-          answerDecode.push(string.charAt (i));
+        if (string.charCodeAt (i) <= 64 || string.charCodeAt (i) > 90)    //Si se insertar valores diferentes a letras o números, no serán codificados
+        {         
+          let positionInitialDecode =arrayNumbersDecode.indexOf(string.charAt (i)); 
+
+          if (positionInitialDecode >= 0) {
+            let positionFinal = positionInitialDecode - (offSet % arrayNumbersDecode.length);
+
+            if (positionFinal < 0){
+             let positionFinalTwo = arrayNumbersDecode.length + positionFinal;                   // si la posición da un número negativo, se resta el numero de elementos del array
+              answerDecode.push (arrayNumbersDecode[positionFinalTwo]); 
+
+            }else {
+              answerDecode.push (arrayNumbersDecode[positionFinal]);                       //Si se insertan valores como números, vocales con tilde o la 'Ñ', se decodifican con un array    
+
+            }
+                  
+          }
+          else 
+          {
+            answerDecode.push(string.charAt (i));                           
+          } 
         } 
         else 
         {
